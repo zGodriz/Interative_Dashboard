8bb0399e47cee28ab48eb85f295dbcb0b8445496
zGodriz <rodrigocauazgodri@gmail.com>
2025-02-27 13:37:09 -0300

    atualização

--- dashboard.py
+++ dashboard.py
@@ -4,5 +4,18 @@ import plotly.express as px
 
 st.set_page_config(layout="wide")
 
+#visão mensal
+#checar dados da planilha
+
 df = pd.read_csv("empresa_invoicesnew.csv", sep=";", decimal=",")
-df
+df["Date"] = pd.to_datetime(df["Date"])
+df = df.sort_values(by="Date")
+
+df["Month"] = df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
+month = st.sidebar.selectbox("Mês", df["Month"].unique())
+
+df_filtered = df[df["Month"] == month]
+df_filtered
+
+col1, col2 = st.columns(2)
+col3, col4, col5 = st.columns(3)
